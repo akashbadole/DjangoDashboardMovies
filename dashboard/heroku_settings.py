427118c230466e20b049164a4adbfc6fda381a09
Akash B <akashbadole@rediffmail.com>
2022-08-27 21:37:38 +0530

    allowed host

--- dashboard/heroku_settings.py
+++ dashboard/heroku_settings.py
@@ -30,4 +30,4 @@ STATICFILES_DIRS = (
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-ALLOWED_HOSTS = [' bookmymoviedjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['bookmymoviedjangoapp.herokuapp.com']
